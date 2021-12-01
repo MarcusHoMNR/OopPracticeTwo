59246f55fcebc300132eba940bc8776eea91087a
Marcus Ho <marcus.ho@oocl.com>
2021-12-01 19:05:28 +0800

    [OopPracticeTwo][Marcus]feat: Task 8.

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,45 @@
 package practice08;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private int number;
+    private Student leader;
+    private List<Student> studentList = new ArrayList<>();
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %d", number);
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public String getLeaderName() {
+        return leader == null ? null : leader.getName();
+    }
+
+    public void assignLeader(Student student) {
+
+        if (studentList.contains(student)) {
+            this.leader = student;
+        } else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public void appendMember(Student student) {
+        this.studentList.add(student);
+    }
+
 }
--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,27 @@
 package practice08;
 
-public class Teacher {
+
+public class Teacher extends Person{
+    private Klass klass;
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        return String.format(super.introduce() + " I am a Teacher. I teach %s.", this.klass == null ? "No Class" : klass.getDisplayName());
+    }
+
+    public String introduceWith(Student student) {
+        return String.format(super.introduce() + " I am a Teacher. I %steach %s.", student.getKlass().getNumber() == klass.getNumber()? "" : "don't ", student.getName());
+    }
 }
