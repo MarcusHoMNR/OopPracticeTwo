0796bfe0813d00508b7253326c61a9c5d4c95408
Marcus Ho <marcus.ho@oocl.com>
2021-12-01 18:47:31 +0800

    [OopPracticeTwo][Marcus]feat: Task 7.

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,30 @@
 package practice07;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %d", number);
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public String getLeaderName() {
+        return leader == null ? null : leader.getName();
+    }
+
+    public void assignLeader(Student student) {
+        this.leader = student;
+    }
 }
--- src/main/java/practice07/Person.java
+++ src/main/java/practice07/Person.java
@@ -1,4 +1,30 @@
 package practice07;
 
 public class Person {
+    private int id;
+    private String name;
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce() {
+        return String.format("My name is %s. I am %d years old.", this.name, this.age);
+    }
+
 }
